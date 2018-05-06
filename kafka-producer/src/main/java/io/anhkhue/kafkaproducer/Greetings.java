a1ef8e7a22235b039ee65b1c4a8529bfef22e586
anh-khue <thailyanhkhue@gmail.com>
2018-05-06 18:27:02 +0700

    Create Message object.

---
+++ kafka-producer/src/main/java/io/anhkhue/kafkaproducer/Greetings.java
@@ -0,0 +1,15 @@
+package io.anhkhue.kafkaproducer;
+
+import lombok.Builder;
+import lombok.Data;
+import lombok.ToString;
+
+@Data
+@ToString
+@Builder
+public class Greetings {
+
+    private long timestamp;
+    private String message;
+
+}
